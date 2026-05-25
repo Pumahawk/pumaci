697ea77463c03f157a42a1233cd6e4f8ae67c30f
Lorenzo Gandino <lorenzoga94@gmail.com>
2026-05-26 00:18:45 +0200

    create log system

---
+++ internal/log/log.go
@@ -0,0 +1,23 @@
+package log
+
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
+var enabled = false
+
+func init() {
+	if v, ok := os.LookupEnv("PUMACI_LOG"); ok {
+		if lv, err := strconv.ParseBool(v); err == nil {
+			enabled = lv
+		}
+	}
+}
+
+func Debug(f string, v ...any) {
+	if enabled {
+		log.Printf(f, v...)
+	}
+}
