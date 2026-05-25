c5ff8ad9a3be71ab969afebbde36446766d7f7e0
Lorenzo Gandino <lorenzoga94@gmail.com>
2026-05-26 00:49:47 +0200

    refactoring tokenization

--- internal/reg/reg.go
+++ internal/reg/reg.go
@@ -1,20 +1,36 @@
 package reg
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
 	"regexp"
 	"strings"
 	"sync"
+
+	"github.com/Pumahawk/pumaci/internal/log"
 )
 
 type Client struct {
-	token string
-	mx    sync.Mutex
+	tokens map[string]string
+	mxTk   sync.RWMutex
+	mx     sync.Mutex
+}
+
+func (c *Client) getTk(scope string) string {
+	c.mxTk.RLock()
+	defer c.mxTk.RUnlock()
+	return c.tokens[scope]
+}
+
+func (c *Client) setTk(scope, token string) {
+	c.mxTk.Lock()
+	defer c.mxTk.Unlock()
+	if c.tokens == nil {
+		c.tokens = make(map[string]string)
+	}
+	c.tokens[scope] = token
 }
 
 func (c *Client) Manifest(image string) (any, error) {
@@ -24,20 +40,31 @@ func (c *Client) Manifest(image string) (any, error) {
 	if err != nil {
 		return nil, fmt.Errorf("parse image %q: %w", image, err)
 	}
+
 	rawUrl, err := url.JoinPath(img.Url, img.Path, "manifests", img.Tag)
 	if err != nil {
 		return nil, err
 	}
+	log.Debug("manifest raw_url=%q", rawUrl)
 	req, err := http.NewRequest("GET", rawUrl, nil)
 	if err != nil {
 		return nil, err
 	}
+
+	scope := getScopeFromImage(img, "pull")
+	log.Debug("lookup tk scope=%q", scope)
+	if tk := c.getTk(scope); tk != "" {
+		log.Debug("find stored token scope=%q", scope)
+		req.Header.Add("authorization", "Bearer "+tk)
+	}
+
 	res, err := cl.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("do request: %w", err)
 	}
 	defer res.Body.Close()
 	if res.StatusCode == 401 {
+		log.Debug("authentication required, status_code=%d", res.StatusCode)
 		tokenize, err := c.tokenize(res.Header.Get("www-authenticate"))
 		if err != nil {
 			return "", err
@@ -60,11 +87,9 @@ func (c *Client) Manifest(image string) (any, error) {
 }
 
 func (c *Client) tokenize(wwwAuth string) (string, error) {
+	log.Debug("start tokenize www=%q", wwwAuth)
 	c.mx.Lock()
 	defer c.mx.Unlock()
-	if c.token != "" {
-		return c.token, nil
-	}
 
 	if !regexp.MustCompile("^Bearer ").MatchString(wwwAuth) {
 		return "", fmt.Errorf("expected brearer wwwAuth=%q", wwwAuth)
@@ -74,7 +99,7 @@ func (c *Client) tokenize(wwwAuth string) (string, error) {
 
 	ps := strings.Split(wwwAuth, ",")
 	rx := regexp.MustCompile("^([A-Za-z]*)=\"([^\"]*)\"")
-	var realm string
+	var realm, scope string
 	urlv := make(url.Values)
 	for _, p := range ps {
 		vs := rx.FindAllStringSubmatch(p, -1)
@@ -85,11 +110,18 @@ func (c *Client) tokenize(wwwAuth string) (string, error) {
 		if len(v) != 3 {
 			return "", fmt.Errorf("unexpected token param groups parsing param=%q size=%d", p, len(v))
 		}
-		if v[1] == "realm" {
-			realm = v[2]
+		key, value := v[1], v[2]
+		if key == "realm" {
+			realm = value
+			log.Debug("find realm=%q", realm)
 			continue
 		}
-		urlv.Add(v[1], v[2])
+		if key == "scope" {
+			scope = value
+			log.Debug("find scope=%q", scope)
+		}
+		log.Debug("set parameter %q=%q", key, value)
+		urlv.Add(key, value)
 	}
 	if realm == "" {
 		return "", fmt.Errorf("missing realm paramater wwwAuth=%q", wwwAuth)
@@ -99,19 +131,19 @@ func (c *Client) tokenize(wwwAuth string) (string, error) {
 		return "", fmt.Errorf("parse realm realm=%q: %w", err)
 	}
 	u.RawQuery = urlv.Encode()
-	res, err := http.Get(u.String())
+	rawUrl := u.String()
+	log.Debug("tokenize rawUrl=%q", rawUrl)
+	res, err := http.Get(rawUrl)
 	if err != nil {
 		return "", err
 	}
 	defer res.Body.Close()
-	bf := &bytes.Buffer{}
-	io.Copy(bf, res.Body)
 	type tok struct{ Token string }
 	dto := &tok{}
-	if err := json.NewDecoder(bf).Decode(dto); err != nil {
+	if err := json.NewDecoder(res.Body).Decode(dto); err != nil {
 		return "", fmt.Errorf("parse response token: %w", err)
 	}
-	c.token = dto.Token
+	c.setTk(scope, dto.Token)
 	return dto.Token, nil
 }
 
@@ -158,4 +190,8 @@ func ParseImage(image string) (*Image, error) {
 		Image: imageName,
 		Tag:   imageTag,
 	}, nil
+}
+
+func getScopeFromImage(image *Image, method string) string {
+	return fmt.Sprintf("repository:%s:%s", image.Path, method)
 }
